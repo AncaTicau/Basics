db858d3ca498afc79fe3d7c652a18748e0a230b0
MariusA <anca.ticau@gmail.com>
2019-06-21 15:52:11 +0300

    Basics exercise2

---
+++ BasicsEx2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicsEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads from the console the radius "r" of a circle and prints its perimeter and area.
+
+            Console.WriteLine("Please what number you want the radius of the circle to be.");
+            int radius = int.Parse(Console.ReadLine());
+            Console.WriteLine("The perimeter of the circle is : {0} and the area is : {1}", 2 * Math.PI * radius, Math.PI * radius * radius);
+        }
+    }
+}
